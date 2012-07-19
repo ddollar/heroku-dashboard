4c938b5540b7ce32ab3551f4ccd46dee8b43c30c
David Dollar <ddollar@gmail.com>
2012-07-19 12:47:13 -0400

    factor out optional_app

--- init.rb
+++ init.rb
@@ -8,13 +8,18 @@ class Heroku::Command::Dashboard < Heroku::Command::Base
   # opens the heroku dashboard for an app
   #
   def index
-    specified_app = app rescue nil
-
-    if specified_app
+    if app = optional_app
       Launchy.open("https://dashboard.heroku.com/apps/#{app}")
     else
       Launchy.open("https://dashboard.heroku.com")
     end
+  end
+
+
+private
+
+  def optional_app
+    app rescue nil
   end
 
 end
